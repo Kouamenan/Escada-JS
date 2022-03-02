98c08ad7baa91b67771518db16e131a1055909ff
Kouamenan <rabogourin@hotmail.com>
2022-03-02 05:11:26 -0300

    first commit

---
+++ escada.js
@@ -0,0 +1,11 @@
+function escada(n) {
+    let filledArray = new Array(n).fill(' ');
+    while (filledArray.indexOf(' ') !== -1) {
+            filledArray.shift();
+            filledArray.push('*');
+            console.log(filledArray.join(""));
+            n--;
+        }
+    }
+
+    escada(6)
